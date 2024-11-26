3f8cac7b508dd16a5b8934a8c97b1c02c9cde2ff
sxlx333 <knifeq8@gmail.com>
2024-11-26 11:47:23 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+const contentDOM = document.getElementById("content");
 const gameDOM = document.getElementById("game");
-gameDOM.classList.add("game");
 
 const maxCellsCount = 100;
-const { clientHeight, clientWidth } = document.body;
+const { clientHeight, clientWidth } = contentDOM;
 const cellSize = Math.floor(
   Math.sqrt((clientHeight * clientWidth) / maxCellsCount)
 );
@@ -13,18 +13,17 @@ gameDOM.style.width = width * cellSize + "px";
 gameDOM.style.height = height * cellSize + "px";
 
 const cellsHTML =
-  `<div class="cell" style="width: ${cellSize}px; height: ${cellSize}px";></div>`.repeat(
+  `<div class="cell" style="width: ${cellSize}px; height: ${cellSize}px;"></div>`.repeat(
     width * height
   );
 gameDOM.innerHTML = cellsHTML;
-
-// const cellsDOM = gameDOM.querySelectorAll(".cell");
 const cellsDOM = gameDOM.getElementsByClassName("cell");
 
 const direction = {
   x: 0,
   y: 0,
 };
+let gameInProgress = true;
 
 function randomLocation() {
   return {
@@ -36,17 +35,72 @@ function randomLocation() {
 function randomAppleLocation() {
   while (true) {
     const location = randomLocation();
-    if (location.x !== snakeLocation.x || location.y !== snakeLocation.y) {
+    let samePoint = false;
+
+    for (const coor of snakeLocation) {
+      if (location.x === coor.x && location.y === coor.y) {
+        samePoint = true;
+        break;
+      }
+    }
+
+    if (!samePoint) {
       return location;
     }
   }
 }
 
-const snakeLocation = randomLocation();
-cellsDOM[snakeLocation.y * width + snakeLocation.x].classList.add("snake");
+function drawSnake() {
+  const nextPoint = {
+    x: snakeLocation.at(-1).x + direction.x,
+    y: snakeLocation.at(-1).y + direction.y,
+  };
 
-const appleLocation = randomAppleLocation();
-cellsDOM[appleLocation.y * width + appleLocation.x].classList.add("apple");
+  if (
+    nextPoint.x < 0 ||
+    nextPoint.y < 0 ||
+    nextPoint.x >= width ||
+    nextPoint.y >= height
+  ) {
+    gameInProgress = false;
+    console.log("GAME OVER!");
+    return;
+  }
+
+  if (snakeLocation.length > 1) {
+    for (const coor of snakeLocation) {
+      if (coor.x === nextPoint.x && coor.y === nextPoint.y) {
+        gameInProgress = false;
+        console.log("GAME OVER!");
+        return;
+      }
+    }
+  }
+
+  for (const coor of snakeLocation) {
+    cellsDOM[coor.y * width + coor.x].classList.remove("snake");
+  }
+
+  snakeLocation.push(nextPoint);
+  snakeLocation.shift();
+
+  for (const coor of snakeLocation) {
+    cellsDOM[coor.y * width + coor.x].classList.add("snake");
+  }
+}
+
+const snakeLocation = [randomLocation()];
+drawSnake();
+
+function drawApple() {
+  const newCoor = randomAppleLocation();
+  cellsDOM[appleLocation.y * width + appleLocation.x].classList.remove("apple");
+  appleLocation.x = newCoor.x;
+  appleLocation.y = newCoor.y;
+  cellsDOM[appleLocation.y * width + appleLocation.x].classList.add("apple");
+}
+const appleLocation = { x: 0, y: 0 };
+drawApple();
 
 addEventListener("keydown", (e) => {
   switch (e.key) {
@@ -73,9 +127,19 @@ addEventListener("keydown", (e) => {
   }
 });
 
-setInterval(() => {
-  cellsDOM[snakeLocation.y * width + snakeLocation.x].classList.remove("snake");
-  snakeLocation.x += direction.x;
-  snakeLocation.y += direction.y;
-  cellsDOM[snakeLocation.y * width + snakeLocation.x].classList.add("snake");
-}, 1000);
+function gameLoop() {
+  if (!gameInProgress) {
+    return;
+  }
+
+  if (
+    snakeLocation.at(-1).x + direction.x === appleLocation.x &&
+    snakeLocation.at(-1).y + direction.y === appleLocation.y
+  ) {
+    snakeLocation.unshift(appleLocation);
+    drawApple();
+  }
+  drawSnake();
+}
+
+setInterval(gameLoop, 300);
